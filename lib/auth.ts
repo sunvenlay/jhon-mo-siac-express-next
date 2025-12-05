21c7f94604f1318cfa7474ef100a82d38d0dce98
sunvenlay <maximo.ventocilla@tecsup.edu.pe>
2025-12-05 13:37:41 -0500

    feat: utilidades y funciones de librería

---
+++ lib/auth.ts
@@ -0,0 +1,66 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
+import { compare } from "bcryptjs"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+        const user = await prisma.usuario.findUnique({
+          where: { email: credentials.email as string },
+        })
+
+        if (!user) {
+          return null
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password as string,
+          user.password
+        )
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.nombre,
+          rol: user.rol,
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = (user as any).rol
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user as any).role = token.role as string
+        (session.user as any).id = token.id as string
+      }
+      return session
+    },
+  },
+})
